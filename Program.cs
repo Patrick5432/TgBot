13f707676ddeb846e4e82a733d0f7924d517821d
IS22-11 <IS22-11@MK-104-021>
2024-09-06 11:12:12 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection.Metadata;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+
+class Program
+{
+    private static ITelegramBotClient? client;
+    private static ReceiverOptions? receiverOptions;
+    private static string token = "7527295657:AAFxiTsCyYn3ROgeJNC8o3D-BFi2dD-gUKk";
+
+    private static void Main()
+    {
+        client = new TelegramBotClient(token);
+        receiverOptions = new ReceiverOptions()
+        {
+           
+        };
+    }
+}
